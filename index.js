3184342b3c86079e034aca7cfeea1460550bfbce
ThelsK <github@thels.nl>
2019-09-06 11:02:38 +0200

    Setup database

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const app = require("express")()
 const port = process.env.PORT || 4000
 
+const database = require("./database")
+
 app.use(
   require("cors")(),
   require("body-parser").json(),
+  // require("./event/router"),
 )
 
 app.listen(port, () => console.log(`Listening on :${port}`))
