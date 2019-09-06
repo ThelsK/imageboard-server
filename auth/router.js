cee583b1219e0b6a7938948ef3b6972febcfbb4b
ThelsK <github@thels.nl>
2019-09-06 13:35:55 +0200

    Added secret route

--- auth/router.js
+++ auth/router.js
@@ -4,7 +4,7 @@ const { toJWT, toData } = require("./jwt")
 jwtRouter.post("/login", (req, res, next) => {
   if (!req.body.email || !req.body.password) {
     res.status(400).send({
-      message: "Please supply a valid email and password"
+      message: "Please supply a valid email and password."
     })
     return
   }
@@ -13,6 +13,30 @@ jwtRouter.post("/login", (req, res, next) => {
   res.send({
     jwt: toJWT({ userId: 1 })
   })
+})
+
+jwtRouter.get("/secret", (req, res, next) => {
+  const auth = req.headers.authorization &&
+    req.headers.authorization.split(' ')
+  if (auth && auth[0] === 'Bearer' && auth[1]) {
+    try {
+      const data = toData(auth[1])
+      res.send({
+        message: 'Thanks for visiting the secret endpoint.',
+        data
+      })
+    }
+    catch (error) {
+      res.status(400).send({
+        message: `Error ${error.name}: ${error.message}`,
+      })
+    }
+  }
+  else {
+    res.status(401).send({
+      message: "Please supply some valid credentials."
+    })
+  }
 })
 
 module.exports = jwtRouter
