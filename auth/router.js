496562a5ad52ea6210334f48d7d5b59241f7d389
ThelsK <github@thels.nl>
2019-09-06 14:19:59 +0200

    Create user and verify login

--- auth/router.js
+++ auth/router.js
@@ -1,18 +1,45 @@
 const jwtRouter = new require("express").Router()
 const { toJWT, toData } = require("./jwt")
+const User = require("../user/model")
 
 jwtRouter.post("/login", (req, res, next) => {
+
   if (!req.body.email || !req.body.password) {
     res.status(400).send({
       message: "Please supply a valid email and password."
     })
     return
   }
-  // Insert proper user validation here, and
-  // Unhardcode the user id provided below.
-  res.send({
-    jwt: toJWT({ userId: 1 })
-  })
+
+  User.findOne({ where: { email: req.body.email } })
+    .then(entity => {
+
+      if (!entity) {
+        res.status(400).send({
+          message: "No user found with that email."
+        })
+        return
+      }
+
+      if (!require("bcrypt").compareSync(req.body.password, entity.password)) {
+        res.status(400).send({
+          message: "Incorrect password for that email."
+        })
+        return
+      }
+
+      res.send({
+        jwt: toJWT({ userId: entity.id })
+      })
+    })
+
+    .catch(error => {
+      console.error(error)
+      res.status(500).send({
+        message: "An unexpected error occurred.",
+        error
+      })
+    })
 })
 
 jwtRouter.get("/secret", (req, res, next) => {
